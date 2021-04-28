7cdae12eac4f4296e23432e45548c054cd3b4385
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2021-04-28 14:00:17 -0400

    Added paths to logs and checkpoints.

--- paths.py
+++ paths.py
@@ -1,12 +1,20 @@
+
+import os
 # local
 PATH_TO_FASTA = '../../Documents/Wang lab/dnabert/ecoli_genome.fasta'
 PATH_TO_GENOMES = 'genomes'
 PATH_TO_TAR = '../../Documents/Wang lab/dnabert/genome_assemblies_3_files.tar'
 
+CHECKPOINTS_DIR = 'checkpoints'
+LOGS_DIR = 'logs'
+
 # server
 '''
 DATA_PATH = '../data/dnabert/'
-PATH_TO_FASTA = DATA_PATH + ''
-PATH_TO_GENOMES = DATA_PATH + 'genomes'
-PATH_TO_TAR = DATA_PATH + ''
+PATH_TO_FASTA = os.path.join(DATA_PATH + '')
+PATH_TO_GENOMES = os.path.join(DATA_PATH, 'genomes')
+PATH_TO_TAR = os.path.join(DATA_PATH + '')
+
+CHECKPOINTS_DIR = os.path.join(PATH_TO_DATA + 'checkpoints')
+LOG_DIR = os.path_join(PATH_TO_DATA, 'logs')
 '''
