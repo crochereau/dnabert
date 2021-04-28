b726976366425271fe270c9f069f8b191cdc2f62
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2021-04-28 15:16:12 -0400

    New file for preprocessing data separately.

---
+++ preprocess_data.py
@@ -0,0 +1,36 @@
+from argparsers import data_parser
+
+from paths import *
+
+# vocabulary
+DNA_TOKS = ['a','c','g','t']
+SPECIAL_TOKS = ['[cls]', '[mask]', '[sep]', '[pad]']
+VOCAB = SPECIAL_TOKS + DNA_TOKS
+
+MAX_SEQ_LEN = 510
+
+args = data_parser.parse_args()
+
+if args.large_dataset == True:
+	# extract files from archive
+	pathlib.Path(PATH_TO_GENOMES).mkdir(parents=True, exist_ok=True)
+	# make genome chunks from multiple genomes
+	all_chunks = process_data_from_tar(PATH_TO_GENOMES, PATH_TO_TAR, MAX_SEQ_LEN)
+else:
+	# make genome chunks from single genome
+	all_chunks = process_data_from_fasta(PATH_TO_FASTA, MAX_SEQ_LEN)
+
+# check that chunks have expected size
+for chunk in all_chunks:
+	assert len(chunk) == MAX_SEQ_LEN
+
+dataset_size = len(all_chunks)*MAX_SEQ_LEN
+print('{:,}'.format(len(all_chunks)), 'chunks ready')
+print('size of dataset:', '{:,}'.format(dataset_size), 'letters')
+
+# Tokenize and vectorize sequences
+toks_to_ids = map_toks_to_ids(VOCAB)
+ids_to_toks = map_ids_to_toks(VOCAB)
+print(toks_to_ids)
+all_ids = process_seq_chunks(all_chunks=all_chunks, toks_to_ids=toks_to_ids)
+print(all_ids.shape)
