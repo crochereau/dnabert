fbf58033643fa69851df50c8ae9c5b5c1449239f
Charlotte Rochereau <Charlotte.rochereau@gmail.com>
2021-04-28 14:57:41 -0400

    Added paths from remote server

--- paths.py
+++ paths.py
@@ -1,20 +1,21 @@
 
 import os
 # local
-PATH_TO_FASTA = '../../Documents/Wang lab/dnabert/ecoli_genome.fasta'
-PATH_TO_GENOMES = 'genomes'
-PATH_TO_TAR = '../../Documents/Wang lab/dnabert/genome_assemblies_3_files.tar'
 
-CHECKPOINTS_DIR = 'checkpoints'
-LOGS_DIR = 'logs'
+#PATH_TO_FASTA = '../../Documents/Wang lab/dnabert/ecoli_genome.fasta'
+#PATH_TO_GENOMES = 'genomes'
+#PATH_TO_TAR = '../../Documents/Wang lab/dnabert/genome_assemblies_3_files.tar'
+
+#CHECKPOINTS_DIR = 'checkpoints'
+#LOGS_DIR = 'logs'
 
 # server
-'''
-DATA_PATH = '../data/dnabert/'
-PATH_TO_FASTA = os.path.join(DATA_PATH + '')
-PATH_TO_GENOMES = os.path.join(DATA_PATH, 'genomes')
-PATH_TO_TAR = os.path.join(DATA_PATH + '')
 
-CHECKPOINTS_DIR = os.path.join(PATH_TO_DATA + 'checkpoints')
-LOG_DIR = os.path_join(PATH_TO_DATA, 'logs')
-'''
+DATA_PATH = '../../data/dnabert/'
+PATH_TO_FASTA = os.path.join(DATA_PATH, 'ecoli_genome.fasta')
+PATH_TO_GENOMES = os.path.join(DATA_PATH, 'genomes')
+PATH_TO_TAR = os.path.join(DATA_PATH, 'genome_assemblies_100_files.tar')
+
+CHECKPOINTS_DIR = os.path.join(DATA_PATH, 'checkpoints')
+LOGS_DIR = os.path.join(DATA_PATH, 'logs')
+
