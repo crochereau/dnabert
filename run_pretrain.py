42b4a2ce097f16be5447898983c3bc9f568bb4ab
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2021-04-28 13:59:48 -0400

    Added train functions.

--- run_pretrain.py
+++ run_pretrain.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 
 from sklearn.model_selection import train_test_split
@@ -32,7 +33,6 @@ args = parser.parse_args()
 if args.large_dataset == True:
 	# extract files from archive
 	pathlib.Path(PATH_TO_GENOMES).mkdir(parents=True, exist_ok=True)
-	extract_tar_file(PATH_TO_TAR, PATH_TO_GENOMES)
 	# make genome chunks from multiple genomes
 	all_chunks = process_data_from_tar(PATH_TO_GENOMES, PATH_TO_TAR, MAX_SEQ_LEN)
 else:
@@ -68,14 +68,69 @@ for inputs_batch, labels_batch in train_ds.take(1):
 	print(inputs_batch)
 	print(labels_batch)
 
+
+# Create checkpoint directory
+checkpoint_dir = os.path.join(CHECKPOINTS_DIR, args.exp_name)
+checkpoint_path = os.path.join(checkpoint_dir, "cp-{epoch:08d}.ckpt")
+
+print("Checkpoints directory is", checkpoint_dir)
+if os.path.exists(checkpoint_dir):
+	print("Checkpoints folder already exists")
+else:
+	print("Creating a checkpoints directory")
+	os.makedirs(checkpoint_dir)
+
+# Restore latest checkpoint (if it exists)
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+if latest != None:
+	print("Loading weights from", latest)
+	model.load_weights(latest)
+else:
+	print("Checkpoint not found. Starting from scratch")
+
+# Create log directory
+logs = os.path.join(LOGS_DIR, datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+
 # Import model
 #DEVICE = "/gpu:0"
 
-# divide config params by 2, to run model quickly
-config = BertConfig(vocab_size=len(VOCAB), num_hidden_layers=args.n_hidden_layers,
-                    num_attention_heads=args.n_attention_heads, hidden_size=args.hidden_size,
-                    intermediate_size=args.intermediate_size)
+config = BertConfig(
+	vocab_size=len(VOCAB),
+	num_hidden_layers=args.n_hidden_layers,
+	num_attention_heads=args.n_attention_heads,
+	hidden_size=args.hidden_size,
+	intermediate_size=args.intermediate_size)
+
 model = TFBertForMaskedLM(config)
 
 configuration = model.config
 print(configuration)
+
+# training
+lr_schedule = tf.keras.optimizers.schedules.PolynomialDecay(
+	initial_learning_rate=2e-5,
+	decay_steps=15,
+	end_learning_rate=0)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+
+my_callbacks = [
+	tf.keras.callbacks.ModelCheckpoint(
+		filepath=checkpoint_path),
+	tf.keras.callbacks.TensorBoard(
+		log_dir=logs,
+		histogram_freq=1,
+		profile_batch='500,520'),
+]
+
+model.compile(
+	optimizer=optimizer,
+	loss=masked_sparse_cce,
+	metrics=[masked_sparse_ca])
+
+history = model.fit(
+	train_ds,
+	validation_data=val_ds,
+	epochs=args.epochs,
+	callbacks=my_callbacks)
