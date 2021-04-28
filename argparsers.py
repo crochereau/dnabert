3dd9f399a0f9f2f799f4f0be4d78c32dd9aff604
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2021-04-28 15:15:32 -0400

    New parser for data preprocessing.

--- argparsers.py
+++ argparsers.py
@@ -1,51 +1,54 @@
 import argparse
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument(
-	"--large_dataset",
-	default=False,
-	type=bool,
-	required=False,
-	help="Large or small training dataset."
-)
+data_parser = argparse.ArgumentParser()
 
-parser.add_argument(
+data_parser.add_argument(
 	"--exp_name",
 	type=str,
 	required=True,
 	help="Name of experiment."
 )
 
-parser.add_argument(
+
+train_parser = argparse.ArgumentParser()
+
+train_parser.add_argument(
+	"--exp_name",
+	type=str,
+	required=True,
+	help="Name of experiment."
+)
+
+train_parser.add_argument(
 	"--batch_size",
 	type=int,
 	default=256,
 	required=False,
 	help='Batch size.')
 
-parser.add_argument(
+train_parser.add_argument(
 	"--epochs",
 	type=int,
 	default=20,
 	required=False,
 	help="Number of epochs.")
 
-parser.add_argument(
+train_parser.add_argument(
 	"--learning_rate",
 	type = float,
 	default=5e-5,
 	required=False,
 	help="Learning rate.")
 
-parser.add_argument(
+train_parser.add_argument(
 	"--output_dir",
 	type=str,
 	required=False,
 	help="The output directory where the model predictions and checkpoints will be written.",
 	)
 
-parser.add_argument(
+train_parser.add_argument(
 	"--n_hidden_layers",
 	type=int,
 	default=6,
@@ -53,7 +56,7 @@ parser.add_argument(
 	help="Number of hidden layers.",
 	)
 
-parser.add_argument(
+train_parser.add_argument(
 	"--n_attention_heads",
 	type=int,
 	default=4,
@@ -61,7 +64,7 @@ parser.add_argument(
 	help="Number of attention heads.",
 	)
 
-parser.add_argument(
+train_parser.add_argument(
 	"--hidden_size",
 	type=int,
 	default=384,
@@ -69,7 +72,7 @@ parser.add_argument(
 	help="bert hidden size.",
 	)
 
-parser.add_argument(
+train_parser.add_argument(
 	"--intermediate_size",
 	type=int,
 	default=1536,
